552fde51ddee1f2044b35396453efde5addc8ae1
Misha Ward <misha.r.ward@gmail.com>
2026-07-03 10:18:05 -0500

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num))  
+                return true;
+        }
+        return false;
+    }
+}
