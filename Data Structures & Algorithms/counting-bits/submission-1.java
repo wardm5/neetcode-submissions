60c20e6ced1787b7ddb3d566ae7a1d034d528b7c
Misha Ward <misha.r.ward@gmail.com>
2026-07-04 12:23:49 -0500

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+
+        for (int i = 1; i <= n; i++)
+            res[i] = res[i / 2] + i % 2;
+
+        return res;
+    }
+}
