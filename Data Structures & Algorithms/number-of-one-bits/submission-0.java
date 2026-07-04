9c893ff516c8feab73d5e0189c3373704312da2d
Misha Ward <misha.r.ward@gmail.com>
2026-07-04 12:28:07 -0500

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n > 0) {
+            // System.out.println(n);
+            res += n % 2 == 1 ? 1 : 0;
+            n /= 2;
+        }
+        return res;
+    }
+}
