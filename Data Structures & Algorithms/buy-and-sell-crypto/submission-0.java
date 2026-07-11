abd4ff4745b6f35b17b3b5e96771c5428992031b
Misha Ward <misha.r.ward@gmail.com>
2026-07-10 22:03:06 -0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, max = Integer.MIN_VALUE;
+        for (int i = prices.length - 1; i >= 0; i--) {
+            max = Math.max(max, prices[i]);
+            profit = Math.max(profit, max - prices[i]);
+        }
+        return profit;
+    }
+}
