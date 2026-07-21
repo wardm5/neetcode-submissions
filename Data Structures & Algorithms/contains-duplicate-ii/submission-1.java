2d29410f9ec0f8736b37002d4378b8a74d548414
Misha Ward <misha.r.ward@gmail.com>
2026-07-21 01:32:47 -0500

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int left = 0, right = left + 1;
+        set.add(nums[left]);
+        for ( ; left < nums.length - 1 && right < nums.length; )
+            if (right - left > k)
+                set.remove(nums[left++]);
+            else if (!set.add(nums[right++]))
+                return true;
+        return false;
+    }
+}
